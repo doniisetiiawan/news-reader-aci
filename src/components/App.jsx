d7c4e1a3a7e5ada789337a6be4f2c69b5195651d
Doni Setiawan <donisetiawan.050@gmail.com>
2019-02-18 15:21:06 +0700

    Add components

---
+++ src/components/App.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import Home from './Home';
+import Article from './Article';
+
+const articleList = filter => props => (
+  <Home {...props} filter={filter} />
+);
+
+const categoryListStyle = {
+  listStyle: 'none',
+  margin: 0,
+  padding: 0,
+  display: 'flex',
+};
+
+const categoryItemStyle = {
+  padding: '5px',
+};
+
+const Local = articleList('local');
+const Global = articleList('global');
+const Tech = articleList('tech');
+const Sports = articleList('sports');
+
+export default connect(state => state.get('App').toJS())(
+  ({ title, links }) => (
+    <BrowserRouter>
+      <main>
+        <h1>{title}</h1>
+        <ul style={categoryListStyle}>
+          {links.map(l => (
+            <li key={l.url} style={categoryItemStyle}>
+              <NavLink
+                exact
+                to={l.url}
+                activeStyle={{ fontWeight: 'bold' }}
+              >
+                {l.name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+        <section>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/local" component={Local} />
+          <Route exact path="/global" component={Global} />
+          <Route exact path="/tech" component={Tech} />
+          <Route exact path="/sports" component={Sports} />
+          <Route exact path="/articles/:id" component={Article} />
+        </section>
+      </main>
+    </BrowserRouter>
+  ),
+);
---
+++ src/components/Home.jsx
@@ -0,0 +1,99 @@
+/* eslint-disable no-undef */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Map } from 'immutable';
+
+const listStyle = {
+  listStyle: 'none',
+  margin: 0,
+  padding: 0,
+};
+
+const listItemStyle = {
+  margin: '0 5px',
+};
+
+const titleStyle = {
+  background: 'transparent',
+  border: 'none',
+  font: 'inherit',
+  cursor: 'pointer',
+  padding: '5px 0',
+};
+
+const emptyMap = Map()
+  .set(true, <li style={listItemStyle}>...</li>)
+  .set(false, null);
+
+class Home extends Component {
+  static defaultProps = {
+    filter: '',
+  };
+
+  static propTypes = {
+    articles: PropTypes.arrayOf(PropTypes.object).isRequired,
+    fetchArticles: PropTypes.func.isRequired,
+    fetchingArticles: PropTypes.func.isRequired,
+    filter: PropTypes.string,
+    toggleArticle: PropTypes.func.isRequired,
+  };
+
+  componentWillMount() {
+    this.props.fetchingArticles();
+    this.props.fetchArticles(this.props.filter);
+  }
+
+  onTitleClick = id => () => this.props.toggleArticle(id);
+
+  render() {
+    const { onTitleClick } = this;
+    const { articles } = this.props;
+
+    return (
+      <ul style={listStyle}>
+        {emptyMap.get(articles.length === 0)}
+        {articles.map(a => (
+          <li key={a.id} style={listItemStyle}>
+            <button
+              type="button"
+              onClick={onTitleClick(a.id)}
+              style={titleStyle}
+            >
+              {a.title}
+            </button>
+            <p style={{ display: a.display }}>
+              <small>
+                <span>{a.summary}</span>
+                <Link to={`articles/${a.id}`}>More...</Link>
+              </small>
+            </p>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default connect(
+  (state, ownProps) => Object.assign(state.get('Home').toJS(), ownProps),
+  dispatch => ({
+    fetchingArticles: () => dispatch({ type: 'FETCHING_ARTICLES' }),
+    fetchArticles: (filter) => {
+      const headers = new Headers();
+      headers.append('Accept', 'application/json');
+
+      fetch(`/api/articles/${filter}`, { headers })
+        .then(resp => resp.json())
+        .then(json => dispatch({
+          type: 'FETCH_ARTICLES',
+          payload: json,
+        }));
+    },
+    toggleArticle: payload => dispatch({
+      type: 'TOGGLE_ARTICLE',
+      payload,
+    }),
+  }),
+)(Home);
