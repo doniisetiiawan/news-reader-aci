4f08a307d1858625bdad03f157182f597eb21122
Doni Setiawan <donisetiawan.050@gmail.com>
2019-02-21 11:44:35 +0700

    Add line breaks

--- src/store/Article.js
+++ src/store/Article.js
@@ -2,7 +2,9 @@ import { fromJS } from 'immutable';
 
 const typeMap = fromJS({
   FETCHING_ARTICLE: state => state.set('full', '...'),
+
   FETCH_ARTICLE: (state, payload) => state.merge(payload),
+
   FETCHING_ARTICLES: state => state
     .clear()
     .set('full', ''),
--- src/store/Home.js
+++ src/store/Home.js
@@ -2,11 +2,13 @@ import { fromJS } from 'immutable';
 
 const typeMap = fromJS({
   FETCHING_ARTICLES: state => state.update('articles', a => a.clear()),
+
   FETCH_ARTICLES: (state, payload) => state.set(
     'articles',
     fromJS(payload)
       .map(a => a.set('display', 'none')),
   ),
+
   TOGGLE_ARTICLE: (state, id) => state.updateIn([
     'articles',
     state
